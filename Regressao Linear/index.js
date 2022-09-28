a458c9cb676f47f0c83c66b3f3e05dd578dd4c54
fscpinheiro <fscpinheiro@gmail.com>
2022-09-28 12:56:09 -0300

    Regressão Linear - Quadrados de X

--- Regressao Linear/index.js
+++ Regressao Linear/index.js
@@ -6,3 +6,15 @@ function produto(x,y){
         temp.push(parseFloat(x[i]) * parseFloat(y[i]));
     return temp;
 }
+
+//CALCULO DOS QUADRADOS DE X
+function quadrados(x){
+    let temp = [];
+    for(let i=0; i<x.length; i++)
+        temp.push(parseFloat(x[i]) * parseFloat(x[i]));
+    return temp;
+}
+console.log("Produto de X e Y");
+console.log(produto([2,4,6],[1,3,5]));
+console.log("Quadrados");
+console.log(quadrados([2,4,6]));
